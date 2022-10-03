14ebb57e435b666d3263f92671c4b3a08b43a4bf
NishizawaYuga <n.sutamare.2002@outlook.jp>
2022-10-03 11:41:05 +0900

    誤字修正

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int Recursive(int n) {
-	if (n <= 1) {
+	if (n <= 0) {
 		return (100);
 	}
-
 	return (Recursive(n - 1) * 2 - 50);
 }
 
 void main() {
-	printf("一般的な賃金体系で5時間：%d\n", 1072 * 5);
-	printf("再起的な賃金体系で5時間：%d", Recursive(5));
+	int time = 8;
+	int total = 0;
+	printf("一般的な賃金体系で%d時間：%d\n",time, 1072 * time);
+	for (int i = 0; i < time; i++) {
+		total += Recursive(i);
+	}
+	printf("再帰的な賃金体系で%d時間：%d",time, total);
 }
