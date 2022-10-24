30e775f271de8de09c897e62effeb9ba180da6c5
NishizawaYuga <k021g1246@g.neec.ac.jp>
2022-10-24 10:07:11 +0900

    vectorとイテレーター ※真の2-1はこちらです

--- main.cpp
+++ main.cpp
@@ -1,55 +1,61 @@
 #include <stdio.h>
-#include <functional>
-#include <Windows.h>
-
-typedef void (*pFunc)(int*, int*);
-
-//結果発表関数
-void Answer(int* numbers, int* answer) {
-
-	if (numbers == answer) {
-		printf("正解！");
-	}
-	else {
-		printf("ハズレ！");
-	}
-}
-
-void SetTime(pFunc p, int numbers, int answer) {
-	Sleep(3);
-
-	//結果発表の関数を呼び出す
-	p(&numbers, &answer);
-}
+#include <vector>
+using namespace std;
 
 int main(void) {
-	int numbers;
-	while (1) {
-		printf("奇数か偶数か、数字でお答えください（奇数なら1,偶数なら2)：");
-		scanf_s("%d", &numbers);
-		if (numbers < 1 || numbers > 2) {
-			printf("\n1か2でお答えください\n");
-		}
-		else {
-			break;
-		}
+	// int型配列 a (要素数:10)
+	//int a[10];
+	// int型の可変長配列 b (要素数:0)
+	//vector<int> b;
+
+	//vector
+
+	//要素数の読み書き
+	//float型の可変長配列 c (要素数:3)
+	vector<float> c = { 0.0f,1.0f,2.0f };
+
+	//添字1の要素を表示（1.0）
+	printf(" %f\n", c[1]);
+
+	//添字1の様子に-1.5を代入
+	c[1] = -1.5f;
+
+	//添字1の要素を表示（-1.5）
+	printf(" %f\n", c[1]);
+
+	//要素数の変更
+	//int型の可変長配列 d (要素数：0)
+	vector<int> d;
+
+	//様子数を10個に変更（空白文字列で初期化される）
+	d.resize(10);
+
+	//要素数を取得
+	int size = d.size();
+
+	//全要素を表示
+	for (int i = 0; i < size; i++) {
+		printf("%d\n", d[i]);
 	}
 
-	//結果ランダム生成
-	int answer = rand();
-	if (answer % 2 == 0) {
-		answer = 2;
-	}
-	else if (answer % 2 == 1) {
-		answer = 1;
-	}
+	////イテレータ
+	//vector<int> e = { 1,2,3 };
 
-	//結果発表
-	pFunc p;
-	p = Answer;
-	SetTime(p, numbers, answer);
-	std::function<int(int)> judge = [=](int i) {return i * answer; };
-	printf("\nサイコロの出目：%d",judge(rand() % 3 + 1));
+	////int型の可変長配列用のイテレータ
+	//vector<int>::iterator it;
 
+	////eの先頭要素を指す
+	//it = e.begin();
+
+	////itが指している要素を表示
+	//cout << *it << endl;
+
+	////次の要素に移動
+	//it++;
+
+	////itが指している要素を表示
+	//cout << *it << endl;
+
+	//system("pause");
 	return 0;
 }
