7271ae8efe2086152150c5a8cfc725283a767004
NishizawaYuga <k021g1246@g.neec.ac.jp>
2022-11-21 09:36:14 +0900

    練習課題 （途中）

--- main.cpp
+++ main.cpp
@@ -1,62 +1,60 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <list>
-using namespace std;
 
-//駅名
-struct StationName {
-	char stationName[30][20] = {
-		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
-		"Uguisudani","Nippori","Tabata","Komagome","Sugamo",
-		"Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo",
-		"Shinjuku","Yoyogi","Harajuku","Ebisu","Meguro","Gotanda",
-		"Osaki","Shinagawa","Tamachi","Hamamatsucho","Shimbashi",
-		"Yurakucho","Nishi-Nippori","Takanawa-GateWay"};
-};
+//単方向リストの構造体の定義
+typedef struct CELL {
+	int val;
+	struct CELL* next;
+}CELL;
+
+//データを追加する関数のプロトタイプ宣言
+void create(CELL* head, int val);
+
+//一覧を表示する関数のプロトタイプ宣言
+void index(CELL* head);
 
 int main() {
-	StationName name;
+	int val = 5;
+	//先頭のセルの宣言
+	CELL head;
+	head.next = nullptr; //Visual Stdioではnextに何らかの値が入った状態で初期化されるので、nullptrを代入する
 
-	list <int> stationNum{ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
-	23,24,25,26};
+	while (true)
+	{
+		scanf_s("% d", &val);
 
-	printf("until 1970\n");
-	for (list<int>::iterator itr = stationNum.begin(); itr != stationNum.end(); itr++) {
-		for (int i = 0; i < 20; i++) {
-			printf("%c", name.stationName[*itr][i]);
-		}
-		printf("\n");
-	}
-	printf("\nuntil 2019\n");
-	for (list<int>::iterator itr = stationNum.begin(); itr != stationNum.end(); itr++) {
-		if (*itr == 7) {
-			itr = stationNum.insert(itr, 27);
-			for (int i = 0; i < 20; i++) {
-				printf("%c", name.stationName[*itr][i]);
-			}
-			printf("\n");
-			++itr;
-		}
-		for (int i = 0; i < 20; i++) {
-			printf("%c", name.stationName[*itr][i]);
-		}
-		printf("\n");
-	}
-	printf("\nsince 2020\n");
-	for (list<int>::iterator itr = stationNum.begin(); itr != stationNum.end(); itr++) {
-		if (*itr == 23) {
-			itr = stationNum.insert(itr, 28);
-			for (int i = 0; i < 20; i++) {
-				printf("%c", name.stationName[*itr][i]);
-			}
-			printf("\n");
-			++itr;
-		}
-		for (int i = 0; i < 20; i++) {
-			printf("%c", name.stationName[*itr][i]);
-		}
-		printf("\n");
-	}
+		//最後尾にセルを追加
+		create(&head, 3);
 
+		//リスト一覧の表示
+		index(&head);
+	}
 
 	return 0;
+}
+
+//セルを新規作成する関数
+void create(CELL* head, int val) {
+	CELL* cell;
+	//新規作成するセル分のメモリを確保する
+	cell = (CELL*)malloc(sizeof(CELL));
+
+	cell->val = val;
+	cell->next = nullptr;
+
+	//最後(最新)のセルのアドレスの1つ目の処理は引数から持ってきた
+	//リストのうち最初のセルのアドレスが該当する
+	while (head->next != nullptr) {
+		head->next = head->next->next;
+	}
+
+	head->next = cell;
+}
+
+//セルの一覧を表示する関数
+void index(CELL* head) {
+	while (head->next != nullptr) {
+		head->next = head->next->next;
+		printf("%d\n", head->next->val);
+	}
 }
