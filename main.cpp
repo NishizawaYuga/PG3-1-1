554d9d2ad5a8c1083d7f52a5a47a2ab0616e684a
NishizawaYuga <k021g1246@g.neec.ac.jp>
2022-12-02 23:19:57 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //単方向リストの構造体の定義
 typedef struct CELL {
-	int val;
+	char word[100];
 	struct CELL* next;
 }CELL;
 
@@ -14,19 +15,31 @@ void create(CELL* head, int val);
 void index(CELL* head);
 
 int main() {
-	int val;
+	char word[100];
 	//先頭のセルの宣言
 	CELL head;
 	head.next = nullptr; //Visual Stdioではnextに何らかの値が入った状態で初期化されるので、nullptrを代入する
 
 	while (true)
 	{
-		printf("好きな数字を入力してください\n");
+		printf("[要素の操作]\n");
+		printf("1.要素の一覧表示\n");
+		printf("2.最後尾に要素の挿入\n");
+		printf("3.最後尾の要素の削除\n\n");
+		printf("-------------------------------------------\n");
+		printf("操作を選択してください\n");
 		scanf_s("%d", &val);
 
+		if (val == 1) {
+			index(&head);
+		}
 		create(&head, val);
-
-		index(&head);
+		printf("\n-------------------------------------------\n");
+		printf("0.初期画面に戻る\n");
+		scanf_s("%d", &val);
+		if (val != 0) {
+			break;
+		}
 	}
 
 	system("pause");
@@ -53,14 +66,22 @@ void create(CELL* head, int val) {
 
 //セルの一覧を表示する関数
 void index(CELL* head) {
-	printf("\n");
-	printf("[");
+	int num = 0;
+	printf("\n[要素一覧の表示]\n");
+	printf("要素一覧:{\n");
 	while (head->next != nullptr) {
 		head = head->next;
-		printf("%d", head->val);
-		if (head->next != nullptr) {
-			printf(",");
+		printf(" %d: """);
+		for (int i = 0; i < strlen(head->word); i++) {
+			printf("%c", head->word[i]);
 		}
+		if (head->next != nullptr) {
+			printf(",\n");
+		}
+		else {
+			printf("\n}");
+		}
+		num++;
 	}
-	printf("]/\n");
+	printf("\n要素数: %d\n",num);
 }
