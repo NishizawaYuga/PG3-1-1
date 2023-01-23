e2384a9bda8e6e96d421b3283a08b1c3b79ff387
NishizawaYuga <k021g1246@g.neec.ac.jp>
2023-01-23 11:20:55 +0900

    SetTimeをラムダ式で表現

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ typedef void (*pFunc)(int*, int*);
 
 //結果発表関数
 void Answer(int* numbers, int* answer) {
-
-	if (numbers == answer) {
+	if (&numbers == &answer) {
 		printf("正解！");
 	}
 	else {
@@ -16,7 +15,7 @@ void Answer(int* numbers, int* answer) {
 }
 
 void SetTime(pFunc p, int numbers, int answer) {
-	Sleep(3);
+	//Sleep(2400);
 
 	//結果発表の関数を呼び出す
 	p(&numbers, &answer);
@@ -47,7 +46,8 @@ int main(void) {
 	//結果発表
 	pFunc p;
 	p = Answer;
-	SetTime(p, numbers, answer);
+	//SetTime(p, numbers, answer);
+	[&]() {Sleep(2400);p(&numbers, &answer); }();
 	std::function<int(int)> judge = [=](int i) {return i * answer; };
 	printf("\nサイコロの出目：%d",judge(rand() % 3 + 1));
 
